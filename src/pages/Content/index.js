9bc47cfd4da48872feaae8f06c04bd3ecf500430
Asel Peiris <asel.p@eyepax.com>
2023-08-02 18:05:27 +0530

    Appending component to dom node.

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,4 +1,12 @@
 import { printLine } from './modules/print';
+import React from 'react';
+import Popup from '../Popup/Popup';
+import { createRoot } from 'react-dom/client';
+
+const domNode = document.createElement('div');
+const root = createRoot(domNode);
+root.render(<Popup />);
+document.body.appendChild(domNode);
 
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
